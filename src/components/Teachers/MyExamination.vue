83b33f61f2d0c1cacb1718f7b529ad0ec9ccca7a
wangpeng <1677157246@qq.com>
2019-09-29 08:54:58 +0800

    增加我的课程

--- src/components/Teachers/MyExamination.vue
+++ src/components/Teachers/MyExamination.vue
@@ -1,21 +1,30 @@
 <template>
   <div>
-    教师端 考试管理
+    <div  class="header">
+      
+    </div>
+    <div class="header-1" style="margin-left: 256px;">
+      <p>我的考试</p>
+      <span>记录我在联想班的考试成绩以及学校的考试成绩各个方面的表现与进步积极向上 努力进取 养成习惯 成长为未来社会栋梁</span>
+    </div>
   </div>
 </template>
 <script>
 export default {
-  name: 'MyExamination',
-  data(){
-    return {
-
-    }
+  name: "MyExamination",
+  data() {
+    return {};
   },
-  methods: {
-
-  },
-  created(){
-
-  }
-}
+  methods: {},
+  created() {}
+};
 </script>
+<style lang="">
+.header {
+  width: 1280px;
+  margin: 0 auto;
+  overflow: hidden;
+  padding-left: 64px;
+}
+
+</style>
