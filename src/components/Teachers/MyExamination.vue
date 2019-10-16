2d1434426b5ee2fe2704a2b60e53ffb6e5dc12d3
wangpeng <1677157246@qq.com>
2019-10-16 20:16:06 +0800

    更新考试管理

--- src/components/Teachers/MyExamination.vue
+++ src/components/Teachers/MyExamination.vue
@@ -18,7 +18,12 @@
         <el-button type="success" @click="routerPush('CreateExam')">发布考试</el-button>
         <el-button type="primary" @click="routerPush('CreateExamPaper')">创建试卷</el-button>
       </el-row>
-      <el-tabs v-model="activeName" @tab-click="handleClick" style="margin-top:20px;" type="border-card">
+      <el-tabs
+        v-model="activeName"
+        @tab-click="handleClick"
+        style="margin-top:20px;"
+        type="border-card"
+      >
         <el-tab-pane label="大前端" name="first">
           <el-tag type="success" style="margin-bottom:10px;margin-left:5px;">大前端</el-tag>
 
@@ -50,11 +55,25 @@
             <el-table-column label="操作">
               <template slot-scope="scope">
                 <el-button size="mini" @click="handleEdit(scope.row)">查看试卷</el-button>
-                <el-button size="mini" type="danger" @click.native.prevent="handleDelete(scope.$index, scope.row,data1,'page1')">删除</el-button>
+                <el-button
+                  size="mini"
+                  type="danger"
+                  @click.native.prevent="handleDelete(scope.$index, scope.row,data1,'page1')"
+                >删除</el-button>
               </template>
             </el-table-column>
           </el-table>
-          <el-pagination hide-on-single-page :key="1" :current-page="+page1" @current-change="currentChange" background layout="prev, pager, next" :page-size="+pageSize" :total="recordsTotal" style='margin-top:10px;'></el-pagination>
+          <el-pagination
+            hide-on-single-page
+            :key="1"
+            :current-page="+page1"
+            @current-change="currentChange"
+            background
+            layout="prev, pager, next"
+            :page-size="+pageSize"
+            :total="recordsTotal"
+            style="margin-top:10px;"
+          ></el-pagination>
         </el-tab-pane>
 
         <el-tab-pane label="移动互联" name="second">
@@ -88,11 +107,25 @@
             <el-table-column label="操作">
               <template slot-scope="scope">
                 <el-button size="mini" @click="handleEdit(scope.row)">查看试卷</el-button>
-                <el-button size="mini" type="danger" @click.native.prevent="handleDelete(scope.$index, scope.row,data2,'page2')">删除</el-button>
+                <el-button
+                  size="mini"
+                  type="danger"
+                  @click.native.prevent="handleDelete(scope.$index, scope.row,data2,'page2')"
+                >删除</el-button>
               </template>
             </el-table-column>
           </el-table>
-          <el-pagination hide-on-single-page :key="2" :current-page="+page2" @current-change="currentChange" background layout="prev, pager, next" :page-size="+pageSize" :total="recordsTotal" style='margin-top:10px;'></el-pagination>
+          <el-pagination
+            hide-on-single-page
+            :key="2"
+            :current-page="+page2"
+            @current-change="currentChange"
+            background
+            layout="prev, pager, next"
+            :page-size="+pageSize"
+            :total="recordsTotal"
+            style="margin-top:10px;"
+          ></el-pagination>
         </el-tab-pane>
 
         <el-tab-pane label="软件开发" name="third">
@@ -126,21 +159,34 @@
             <el-table-column label="操作">
               <template slot-scope="scope">
                 <el-button size="mini" @click="handleEdit(scope.row)">查看试卷</el-button>
-                <el-button size="mini" type="danger" @click.native.prevent="handleDelete(scope.$index, scope.row,data3,'page3')">删除</el-button>
+                <el-button
+                  size="mini"
+                  type="danger"
+                  @click.native.prevent="handleDelete(scope.$index, scope.row,data3,'page3')"
+                >删除</el-button>
               </template>
             </el-table-column>
           </el-table>
-          <el-pagination hide-on-single-page :key="3" :current-page="+page3" @current-change="currentChange" background layout="prev, pager, next" :page-size="+pageSize" :total="recordsTotal" style='margin-top:10px;'></el-pagination>
+          <el-pagination
+            hide-on-single-page
+            :key="3"
+            :current-page="+page3"
+            @current-change="currentChange"
+            background
+            layout="prev, pager, next"
+            :page-size="+pageSize"
+            :total="recordsTotal"
+            style="margin-top:10px;"
+          ></el-pagination>
         </el-tab-pane>
       </el-tabs>
-
     </div>
   </div>
 </template>
 <script>
 export default {
   name: "MyExamination",
-  data () {
+  data() {
     return {
       activeName: "first",
       typeId: "1",
@@ -155,73 +201,76 @@ export default {
       index: 1
     };
   },
-  watch: {
-    
-  },
+  watch: {},
   methods: {
     // 查看试卷详情
     handleEdit(item) {
-      this.$router.push(`/SeeExam/${item.id}`)
+      this.$router.push(`/SeeExam/${item.id}`);
     },
     // 删除试卷功能  巨麻烦的功能。。。
-    handleDelete (index, item, tableData, page) { // index为当前组件在当前页的下标  // item为该组件内容
-      // 查看试卷是否被关联 
-      this.$http.get(`/business/examPlan/paperStatus/` + item.id).then((res) => {
-        if (typeof res.data === 'boolean') {
+    handleDelete(index, item, tableData, page) {
+      // index为当前组件在当前页的下标  // item为该组件内容
+      // 查看试卷是否被关联
+      this.$http.get(`/business/examPlan/paperStatus/` + item.id).then(res => {
+        if (typeof res.data === "boolean") {
           if (res.data) {
-            this.$http.post(`/exam/examPage/delete?id=${item.id}`).then((res) => {
-              if (res.data === '') {
+            this.$http.post(`/exam/examPage/delete?id=${item.id}`).then(res => {
+              if (res.data === "") {
                 // 删除后，如果当前tableData.length==0,则重新跳转上一个页面，并且重新发送请求，请求上一个页面的内容
                 this.recordsTotal--;
-                tableData.splice(index,1); // 删除当前列表条目
+                tableData.splice(index, 1); // 删除当前列表条目
                 if (tableData.length == 0) {
-                  this.$http.post('/exam/examPage/page', {
-                    pageSize: this.pageSize,
-                    page: this[page] - 1, // 此处需要减1
-                    params: {
-                      typeId: this.index
-                    }
-                  }).then((res) => {
-                    this[`data${this.index}`] = res.data.data;
-                  })
+                  this.$http
+                    .post("/exam/examPage/page", {
+                      pageSize: this.pageSize,
+                      page: this[page] - 1, // 此处需要减1
+                      params: {
+                        typeId: this.index
+                      }
+                    })
+                    .then(res => {
+                      this[`data${this.index}`] = res.data.data;
+                    });
                 }
                 this.$message({
-                  type: 'success',
-                  message: '删除成功'
-                })
+                  type: "success",
+                  message: "删除成功"
+                });
               }
-            })
+            });
           } else {
             this.$message({
-              type: 'warning',
-              message: '当前试卷已被考试关联，无法删除'
-            })
+              type: "warning",
+              message: "当前试卷已被考试关联，无法删除"
+            });
           }
         } else {
-          this.$message.error('网络错误')
+          this.$message.error("网络错误");
         }
-      })
+      });
     },
     // 按钮路由跳转功能
-    routerPush (route) {
+    routerPush(route) {
       this.$router.push(route);
     },
     // 切换页码功能
-    currentChange (val) {
+    currentChange(val) {
       // console.log(val)
       var app = this;
       this[`page${app.index}`] = val;
-      this.$http.post('/exam/examPage/page', {
-        pageSize: this.pageSize,
-        page: val,
-        params: {
-          typeId: this.index
-        }
-      }).then(function (res) {
-        app[`data${app.index}`] = res.data.data;
-      })
+      this.$http
+        .post("/exam/examPage/page", {
+          pageSize: this.pageSize,
+          page: val,
+          params: {
+            typeId: this.index
+          }
+        })
+        .then(function(res) {
+          app[`data${app.index}`] = res.data.data;
+        });
     },
-    handleClick (tab, event, row, index) {
+    handleClick(tab, event, row, index) {
       var index = Number(tab.index) + 1;
       this.index = index;
       if (index == 1) {
@@ -234,7 +283,7 @@ export default {
               typeId: index //试卷类型 typeId  1为大前端  2是移动互联 3是软件开发
             }
           })
-          .then(function (res) {
+          .then(function(res) {
             app.recordsTotal = res.data.recordsTotal;
             app.data1 = res.data.data;
           });
@@ -248,7 +297,7 @@ export default {
               typeId: index //试卷类型 typeId  1为大前端  2是移动互联 3是软件开发
             }
           })
-          .then(function (res) {
+          .then(function(res) {
             app.recordsTotal = res.data.recordsTotal;
             app.data2 = res.data.data;
           });
@@ -262,7 +311,7 @@ export default {
               typeId: index //试卷类型 typeId  1为大前端  2是移动互联 3是软件开发
             }
           })
-          .then(function (res) {
+          .then(function(res) {
             app.recordsTotal = res.data.recordsTotal;
             app.data3 = res.data.data;
           });
@@ -270,7 +319,7 @@ export default {
     }
   },
 
-  created () {
+  created() {
     var app = this;
     this.$http
       .post("/exam/examPage/page", {
@@ -280,7 +329,7 @@ export default {
           typeId: this.typeId //试卷类型 typeId  1为大前端  2是移动互联 3是软件开发
         }
       })
-      .then(function (res) {
+      .then(function(res) {
         app.recordsTotal = res.data.recordsTotal;
         app.data1 = res.data.data;
       });
